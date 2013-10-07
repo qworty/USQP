20623e0f2765f98bbdba80bd6f305e3229af3287
Tobias 'MegaBak' Oelen <tobias@egobeer.nl>
2013-10-08 00:20:52 +0200

    printer module

--- htdocs/index.pl
+++ htdocs/index.pl
@@ -2,3 +2,23 @@
 use strict;
 use warnings FATAL=>'all';
 use DBI;
+use CGI;
+
+my $cgi = new CGI;
+my @buffer;
+
+site($cgi->header());
+
+site("foo bar test");
+
+printSite();
+
+sub site{
+  push @buffer, $_[0];
+}
+sub printSite{
+  for my $line(@buffer){
+    print $line;
+  }
+  @buffer = ();
+}
