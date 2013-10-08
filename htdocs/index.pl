8b58636170fb7c27b5f8228f2bb0b61de0dad64e
Tobias 'MegaBak' Oelen <tobias@egobeer.nl>
2013-10-08 02:36:11 +0200

    players

--- htdocs/index.pl
+++ htdocs/index.pl
@@ -47,7 +47,9 @@ sub selectPlayers{
   $sth_players->execute();
   while(my $player = $sth_players->fetchrow_hashref() ){
     $players{$player->{id}} = $player;
+    site("<pre>");
     site(Dumper $player);
+    site("</pre>");
   }
   return %players;
 }
