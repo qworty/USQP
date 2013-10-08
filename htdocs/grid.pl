69d489d26c1e476f9206ea60cb8fee2bb862b76f
SED9008 <neural@se-designs.nl>
2013-10-08 02:05:16 +0200

    objectplacement

--- htdocs/grid.pl
+++ htdocs/grid.pl
@@ -18,7 +18,8 @@ my @grid;
 site($cgi->header());
 site(printHeader());
 
-randomWalls(300);
+iniPlayers(3);
+randomObjects(300, 'wall');
 
 buildGrid();
 
@@ -26,17 +27,39 @@ site(printFooter());
 
 printSite();
 
-sub randomWalls{
+sub iniPlayers{
+	my $amount	= $_[0];
+	randomObjects($mount,'user');
+}
+
+sub randomObjects{
 	my $amount  = $_[0];
+	my $object 	= $_[1];
 	my @walls;
 	for(my $wall_nr = 0; $wall_nr <= $amount; $wall_nr++){
-		$grid[int(rand($grid_size{'x'}))][int(rand($grid_size{'y'}))] = 'wall';
+		my $x = int(rand($grid_size{'x'}));
+		my $y = int(rand($grid_size{'y'}));
+		if(checkObject($x,$y)){
+			$grid[][] = $object;	
+		} else {
+			$wall_nr --; #try again
+		}	
 	} 
+}
+
+sub checkObject{
+	my $x		= $_[0];
+	my $y		= $_[1];
+	if(defined($grid[$x][$y])){
+		return 0;
+	} else {
+		return 1;
+	}
 }
 
 sub buildGrid{
 	my $width = $grid_size{'x'}*10;
-	site('<div style="text-align:center;border:2px solid;"><div style="border:1px solid;text-align:center;width:'.$width.';">');
+	site('<div style="border:1px solid;text-align:center;width:'.$width.';">');
 	for(my $x = 0; $x < $grid_size{'x'}; $x++){
 		site('<div>');
 		for(my $y = 0; $y < $grid_size{'y'}; $y++){
@@ -44,11 +67,13 @@ sub buildGrid{
 				site('<div style="height:10px;width:10px;display:inline-block;background-color:white;"></div>');
 			} elsif($grid[$x][$y] eq 'wall'){
 				site('<div style="height:10px;width:10px;background-color:grey;display:inline-block;"></div>');
+			} elsif($grid[$x][$y] eq 'user'){
+				site('<div style="height:10px;width:10px;background-color:yellow;display:inline-block;"></div>');
 			}
 		}
 		site('</div>');
 	}
-	site('</div></div>');
+	site('</div>');
 }
 
 sub printHeader{
